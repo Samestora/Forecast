b7a785caab1b389b0fb807fff3ead471b42f39fe
Samestora <putrantosurya38@gmail.com>
2023-08-23 14:02:14 +0700

    bug fix and minor improvements

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
-import preload
 import core
+
+def Get_API():
+	with open("API.txt", "w") as wAPI:
+		API = str(input("Type in your API : "))
+		wAPI.write(API)
+
+def Read_API():
+	with open("API.txt", "r") as reader:
+		API = reader.readline()
+		core.Weather(API)
+
+def Flush_API():
+	with open("API.txt", "w") as clear:
+		clear.write("")
 
 # check Get_API already exist with a valid api or not
 try: 
